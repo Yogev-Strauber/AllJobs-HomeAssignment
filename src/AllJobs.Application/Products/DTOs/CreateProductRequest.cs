ede4c722184f57cd7aba0d8f892862315fb9b6d1
Yogev Strauber <yogev.strauber@gmail.com>
2026-09-16 13:32:56 +0300

    add data Data Annotations

--- src/AllJobs.Application/Products/DTOs/CreateProductRequest.cs
+++ src/AllJobs.Application/Products/DTOs/CreateProductRequest.cs
@@ -1,14 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AllJobs.Application.Products.DTOs;
 
 public class CreateProductRequest
 {
+
+    [Required]
     public required string Name { get; set; }
 
+    [Required]
     public required string Sku { get; set; }
 
     public string? Description { get; set; }
 
+    [Range(0.01, double.MaxValue)]
     public decimal Price { get; set; }
+
+    [Range(0, int.MaxValue)]
 
     public int StockQuantity { get; set; }
 }
